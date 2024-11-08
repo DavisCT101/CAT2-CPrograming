eb7896ef96896524f1c1c5ddb7802bb422fc074e
DavisCT101 <muchekedavis3@gmail.com>
2024-11-08 17:06:30 +0300

    Update CAT2_Q2.c

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -4,17 +4,16 @@ Name: Mucheke Davis Mwangi
 #include <stdio.h> 
 int main(){
     // Declare and initialize the 2D array
-    int scores[2][2] = {
-        {6592, 3570},
-        {8472, 5967}
+    int scores[4][2] = {
+        {65,92},{35,70},
+        {84,72},{59,67}
     };
 
     // Use nested for loops to print the elements
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 2; j++) {
-            printf("%d ", scores[i][j]); // Print each element
+            printf("scores [%d][%d]=%d\n",i,j,scores[i][j]); 
         }
-        printf("\n"); // New line after each row
     }
 
     return 0;
